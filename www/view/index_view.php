618a2c743efd3448f08161a4cc73ce049b7bbba4
Saya Shishido <minm37412@gmail.com>
2019-10-19 09:51:29 +0900

    #6　残りのphpにh関数を追加しました

--- www/view/index_view.php
+++ www/view/index_view.php
@@ -4,7 +4,7 @@
   <?php include VIEW_PATH . 'templates/head.php'; ?>
   
   <title>商品一覧</title>
-  <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'index.css'); ?>">
+  <link rel="stylesheet" href="<?php print(h(STYLESHEET_PATH . 'index.css')); ?>">
 </head>
 <body>
   <?php include VIEW_PATH . 'templates/header_logined.php'; ?>
@@ -20,16 +20,16 @@
         <div class="col-6 item">
           <div class="card h-100 text-center">
             <div class="card-header">
-              <?php print($item['name']); ?>
+              <?php print(h($item['name'])); ?>
             </div>
             <figure class="card-body">
-              <img class="card-img" src="<?php print(IMAGE_PATH . $item['image']); ?>">
+              <img class="card-img" src="<?php print(h(IMAGE_PATH . $item['image'])); ?>">
               <figcaption>
-                <?php print(number_format($item['price'])); ?>円
+                <?php print(h(number_format($item['price']))); ?>円
                 <?php if($item['stock'] > 0){ ?>
                   <form action="index_add_cart.php" method="post">
                     <input type="submit" value="カートに追加" class="btn btn-primary btn-block">
-                    <input type="hidden" name="item_id" value="<?php print($item['item_id']); ?>">
+                    <input type="hidden" name="item_id" value="<?php print(h($item['item_id'])); ?>">
                   </form>
                 <?php } else { ?>
                   <p class="text-danger">現在売り切れです。</p>
