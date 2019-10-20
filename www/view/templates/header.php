383d7514cf1a67c1541735b7cad6864773503e5a
Saya Shishido <minm37412@gmail.com>
2019-10-20 17:17:41 +0900

    #6 header.php、login_view.php、header_logined.phpのコメントを追加しました

--- www/view/templates/header.php
+++ www/view/templates/header.php
@@ -1,16 +1,16 @@
 <header>
   <nav class="navbar navbar-expand-sm navbar-light bg-light">
-    <a class="navbar-brand" href="<?php print(HOME_URL);?>">Market</a>
+    <a class="navbar-brand" href="<?php print(h(HOME_URL));?>">Market</a>
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#headerNav" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="ナビゲーションの切替">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="headerNav">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link" href="<?php print(SIGNUP_URL);?>">サインアップ</a>
+          <a class="nav-link" href="<?php print(h(SIGNUP_URL));?>">サインアップ</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?php print(LOGIN_URL);?>">ログイン</a>
+          <a class="nav-link" href="<?php print(h(LOGIN_URL));?>">ログイン</a>
         </li>
       </ul>
     </div>
