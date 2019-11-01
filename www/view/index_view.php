c1632b3f381840181d25e3a3396037724d125bdc
macmilan <macmilandkrn2019@icloud.com>
2019-11-01 22:35:40 +0900

    商品名のエスケープ処理追加

--- www/view/index_view.php
+++ www/view/index_view.php
@@ -20,7 +20,8 @@
         <div class="col-6 item">
           <div class="card h-100 text-center">
             <div class="card-header">
-              <?php print($item['name']); ?>
+              <!-- エスケープ処理追加　issue＃3-->
+              <?php print(esc($item['name'])); ?>
             </div>
             <figure class="card-body">
               <img class="card-img" src="<?php print(IMAGE_PATH . $item['image']); ?>">
