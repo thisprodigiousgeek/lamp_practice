ba983ed911de2e411aedb2f75a9584c3d87e484e
fujiiree120 <fujiiree120@gmail.com>
2019-11-04 20:42:51 +0900

    .model/function.php 140行目strip_tagsを追加しました

--- www/model/functions.php
+++ www/model/functions.php
@@ -136,5 +136,7 @@ function is_valid_upload_image($image){
 }
 //エスケープ処理の追加
 function h($name){
-  return htmlspecialchars($name, ENT_QUOTES, "UTF-8");
+   htmlspecialchars($name, ENT_QUOTES, "UTF-8");
+   $result = strip_tags($name);
+   return $result;
 }
