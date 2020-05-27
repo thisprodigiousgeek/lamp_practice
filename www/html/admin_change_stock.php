ccc7a9e773b251053c5aeb208065055252768262
EC2 Default User <ec2-user@ip-172-31-86-55.ec2.internal>
2020-05-27 18:02:07 +0000

    floatを消した

--- www/html/admin_change_stock.php
+++ www/html/admin_change_stock.php
@@ -20,7 +20,7 @@ if(is_admin($user) === false){
 
 $item_id = get_post('item_id');
 //stockが数字かどうか、整数かどうかの確認とエスケープ
-if(is_numeric(get_post('stock')) === TRUE && is_float(get_post('stock')) === TRUE){
+if(is_numeric(get_post('stock')) === TRUE){
   $stock = htmlspecialchars(get_post('stock'),ENT_QUOTES,'UTF-8');
 }else{
   set_message('在庫数の変更に失敗しました');
