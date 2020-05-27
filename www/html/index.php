5bbc1c1d3d3bbea8c132596329c82c8486c31a51
EC2 Default User <ec2-user@ip-172-31-86-55.ec2.internal>
2020-05-27 12:30:42 +0000

    一回戻そう

--- www/html/index.php
+++ www/html/index.php
@@ -12,11 +12,6 @@ if(is_logined() === false){
 
 $db = get_db_connect();
 $user = get_login_user($db);
-$items = get_open_items($db);
-
-function h($items){
-    $items = htmlspecialchars($items , ENT_QUOTES , 'UTF-8');
-}
-h($items);
+$items = htmlspecialchars(get_open_items($db),ENT_QUOTES,'UTF-8');
 
 include_once VIEW_PATH . 'index_view.php';
