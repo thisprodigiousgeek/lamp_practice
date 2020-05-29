7944e29728628354495db620aaecacda09ce6a30
EC2 Default User <ec2-user@ip-172-31-86-55.ec2.internal>
2020-05-29 05:00:21 +0000

    SQLインジェクション処理

--- www/html/admin_change_stock.php
+++ www/html/admin_change_stock.php
@@ -20,13 +20,6 @@ if(is_admin($user) === false){
 
 $item_id = get_post('item_id');
 
-//stockが数字かどうか、整数かどうかの確認とエスケープ
-if(is_numeric(get_post('stock')) === TRUE && is_float(get_post('stock')) === FALSE && get_post('stock') > 0){
-  $stock = htmlspecialchars(get_post('stock'),ENT_QUOTES,'UTF-8');
-}else{
-  set_message('在庫数の変更に失敗しました');
-}
-
 if(update_item_stock($db, $item_id, $stock)){
   set_message('在庫数を変更しました。');
 } else {
