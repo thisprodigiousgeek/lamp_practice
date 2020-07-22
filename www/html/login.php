9dd63b9e8113f7b408e6ecb774c33453523f1d70
payamon <hy23.ts.like@gmail.com>
2020-07-22 14:48:00 +0900

    作成途中。

--- www/html/login.php
+++ www/html/login.php
@@ -8,4 +8,6 @@ if(is_logined() === true){
   redirect_to(HOME_URL);
 }
 
+$token = get_csrf_token();
+
 include_once VIEW_PATH . 'login_view.php';
--- www/view/login_view.php
+++ www/view/login_view.php
@@ -21,6 +21,7 @@
         <label for="password">パスワード: </label>
         <input type="password" name="password" id="password" class="form-control">
       </div>
+      <input type="hidden" name="token" value="<?php print $token;?>">
       <input type="submit" value="ログイン" class="btn btn-primary">
     </form>
   </div>
