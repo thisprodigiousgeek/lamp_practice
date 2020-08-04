f0bd8327451783ed8d3b7eb9b8c7511980e7ec1c
payamon <hy23.ts.like@gmail.com>
2020-08-04 12:09:46 +0900

    no message

--- www/view/templates/header_logined.php
+++ www/view/templates/header_logined.php
@@ -12,22 +12,12 @@
         <li class="nav-item">
           <a class="nav-link" href="<?php print(LOGOUT_URL);?>">ログアウト</a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php print(ORDER_URL);?>">購入履歴</a>
+          </li>
         <?php if(is_admin($user)){ ?>
           <li class="nav-item">
             <a class="nav-link" href="<?php print(ADMIN_URL);?>">管理</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php print(ORDER_ADMIN_URL);?>">購入履歴(管理用)</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php print(ORDER_DETAIL_ADMIN_URL);?>">購入明細(管理用)</a>
-          </li>
-        <?php }else{ ?>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php print(ORDER_NORMAL_URL);?>">購入履歴</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php print(ORDER_DETAIL_NORMAL_URL);?>">購入明細</a>
           </li>
         <?php } ?>
       </ul>
