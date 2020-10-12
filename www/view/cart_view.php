84ade67a26747e61aaaee4214e67e208f5c2123a
gotounobuteru <gotonobuteru@gmail.com>
2020-10-12 17:54:49 +0900

    #3

--- www/view/cart_view.php
+++ www/view/cart_view.php
@@ -28,7 +28,7 @@
           <?php foreach($carts as $cart){ ?>
           <tr>
             <td><img src="<?php print(IMAGE_PATH . $cart['image']);?>" class="item_image"></td>
-            <td><?php print($cart['name']); ?></td>
+            <td><?php print h($cart['name']); ?></td>
             <td><?php print(number_format($cart['price'])); ?>円</td>
             <td>
               <form method="post" action="cart_change_amount.php">
