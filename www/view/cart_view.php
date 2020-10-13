04fe40a380107daacd511d85d698b2c98355dcea
Hiroyuki Abe <vanica.zuey@gmail.com>
2020-10-13 21:51:58 +0900

    #3 cart_change_amount.php token 発行処理実装

--- www/view/cart_view.php
+++ www/view/cart_view.php
@@ -36,6 +36,7 @@
                 個
                 <input type="submit" value="変更" class="btn btn-secondary">
                 <input type="hidden" name="cart_id" value="<?php print($cart['cart_id']); ?>">
+                <input type="hidden" name="token" value="<?php print $token; ?>">
               </form>
             </td>
             <td><?php print(number_format($cart['price'] * $cart['amount'])); ?>円</td>
