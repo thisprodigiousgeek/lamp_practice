e637f55154d4f1d4457b6ecf620bad0c6d04e7a0
Hiroyuki Abe <vanica.zuey@gmail.com>
2020-10-17 14:15:43 +0900

    append_schema.sqlの修正

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -9,10 +9,10 @@ CREATE TABLE history(
 -- 購入明細画面テーブル
 CREATE TABLE details(
   purchased_details_id INT AUTO_INCREMENT,
+  purchased_history_id INT,
   item_id INT,
   amount INT DEFAULT 0,
   price_sum INT DEFAULT 0,
-  created DATETIME,
   primary key(purchased_details_id)
 );
 
