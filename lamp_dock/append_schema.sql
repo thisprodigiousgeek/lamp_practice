7c9d3cb60fc20ebc1563b063270cf7eb7d630626
shimizu6660 <b1205880@planet.kanazawa-it.ac.jp>
2020-10-20 18:45:53 +0900

    購入履歴・購入明細画面テーブルのSQL文の作成及テーブルの作成

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,22 +1,22 @@
 --購入履歴画面では「注文番号」「user_id」「合計金額」「購入日時」--
 $sql = "
     CREATE TABLE order_history(
-        order_id_history(11) int NOT NULL AUTO_INCREMENT,
-        user_id(11) int NOT NULL,
-        total_price(11) int NOT NULL,
+        order_id_history int(11) NOT NULL AUTO_INCREMENT,
+        user_id int(11) NOT NULL,
+        total_price int(11) NOT NULL,
         created datetime,
-        primarykey(order_id_history)
+        primary key(order_id_history)
     )
 ";
 --購入明細画面では「注文番号」「購入明細の個別番号」「item_id」「金額」「購入数」--
 $sql = "
     CREATE TABLE order_details(
-        order_details_id(11) int NOT NULL AUTO_INCREMENT,
-        order_id_history(11) int NOT NULL,
+        order_details_id int(11) NOT NULL AUTO_INCREMENT,
+        order_id_history int(11) NOT NULL,
         item_id int(11) NOT NULL,
         price int(11) NOT NULL,
         amount int(11) NOT NULL,
         created datetime,
-        primarykey(order_details_id)
+        primary key(order_details_id)
     )
 ";
