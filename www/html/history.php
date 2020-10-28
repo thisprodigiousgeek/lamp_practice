e0ae526ccf841ba03b46e2c009049dc4ed85548d
Hiroyuki Abe <vanica.zuey@gmail.com>
2020-10-28 15:26:48 +0900

    history.phpの作成

---
+++ www/html/history.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../conf/const.php';
+require_once MODEL_PATH . 'functions.php';
+require_once MODEL_PATH . 'user.php';
+require_once MODEL_PATH . 'cart.php';
+
+session_start();
+
+if(is_logined() === false){
+  redirect_to(LOGIN_URL);
+}
+$token = get_csrf_token();
+
+$db = get_db_connect();
+$user = get_login_user($db);
+
+
+include_once VIEW_PATH . 'history_view.php';
