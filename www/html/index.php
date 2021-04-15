3adc96f0649b6fbaadb989dfcaf1bb71d750fe34
水野凌輔 <ryosuke12021gmail.com>
2021-04-15 22:47:22 +0900

    #3

--- www/html/index.php
+++ www/html/index.php
@@ -4,15 +4,19 @@ require_once MODEL_PATH . 'functions.php';
 require_once MODEL_PATH . 'user.php';
 require_once MODEL_PATH . 'item.php';
 
+//セッションスタート
 session_start();
 
+//ログインされていない状態ならばログイン画面にリダイレクト
 if(is_logined() === false){
   redirect_to(LOGIN_URL);
 }
 
+//DB接続
 $db = get_db_connect();
+//ユーザー情報を取得
 $user = get_login_user($db);
-
+//公開中の商品情報を取得
 $items = get_open_items($db);
 
 include_once VIEW_PATH . 'index_view.php';
--- www/html/index_add_cart.php
+++ www/html/index_add_cart.php
@@ -5,18 +5,23 @@ require_once MODEL_PATH . 'user.php';
 require_once MODEL_PATH . 'item.php';
 require_once MODEL_PATH . 'cart.php';
 
+//セッションスタート
 session_start();
 
+//ログインされていない状態ならばログイン画面にリダイレクト
 if(is_logined() === false){
   redirect_to(LOGIN_URL);
 }
 
+//DB接続
 $db = get_db_connect();
+//ユーザー情報を取得
 $user = get_login_user($db);
 
-
+//post送信されたitem_idを取得
 $item_id = get_post('item_id');
 
+//カート追加処理
 if(add_cart($db,$user['user_id'], $item_id)){
   set_message('カートに商品を追加しました。');
 } else {
