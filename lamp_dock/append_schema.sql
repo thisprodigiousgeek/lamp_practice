02f629c5f0d58585c747c463c6c24ff87ea4f4c3
“Koya <on.the.edge-99@i.softbank.jp>
2021-04-22 19:43:56 +0900

    #10

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -2,7 +2,6 @@
 CREATE TABLE orders (
 order_id int(11) AUTO_INCREMENT,
 user_id int(11) NOT NULL,
-total int(11) NOT NULL,
 created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 primary KEY(order_id),
 FOREIGN KEY(user_id) REFERENCES users(user_id) 
@@ -13,9 +12,8 @@ CREATE TABLE details (
 details_id int(11) AUTO_INCREMENT,
 order_id int(11) NOT NULL,
 item_id int(11) NOT NULL,
-purchase_price int(11) NOT NULL,
-purchase_amount int(11) NOT NULL,
-created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+price int(11) NOT NULL,
+amount int(11) NOT NULL,
 primary key(details_id),
 FOREIGN KEY(order_id) REFERENCES orders(order_id),
 FOREIGN KEY(item_id) REFERENCES items(item_id)
@@ -24,4 +22,3 @@ FOREIGN KEY(item_id) REFERENCES items(item_id)
 -- インデックスの作成
 CREATE INDEX user_id ON orders(user_id);
 CREATE INDEX order_id ON details(order_id);
-CREATE INDEX item_id ON details(item_id);
