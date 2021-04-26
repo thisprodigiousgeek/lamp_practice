24bae68e476e87d11243249d8edda98d70d3d02f
“Koya <on.the.edge-99@i.softbank.jp>
2021-04-27 07:29:23 +0900

    #15

---
+++ www/html/assets/javascript/eventlistener.js
@@ -0,0 +1,8 @@
+
+var select = document.querySelector("#sort");
+
+var options = document.querySelectorAll("#sort option");
+
+select.addEventListener('change', function(){
+    var index = this.selectedIndex;
+}, false)
