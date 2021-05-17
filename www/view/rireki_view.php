6bb890da02a45e35c7e2994e59372e5a3de11af4
yuuki watanabe <atom1991224@gmail.com>
2021-05-17 23:46:42 +0900

    #7

--- www/view/rireki_view.php
+++ www/view/rireki_view.php
@@ -1,9 +1,19 @@
 <!--
-    CREATE history (
+    購入履歴テーブル
+    CREATE TABLE history (
         order_id INT(11) AUTO_INCREMENT,
         user_id INT(11),
         created datetime, 
-        primary key(order_id),
+        primary key(order_id)
+    );
+
+    購入明細テーブル
+    CREATE TABLE detail (
+        order_id INT(11) AUTO_INCREMENT,
+        item_id INT(11),
+        amount INT(11),
+        created datetime,
+        primary key(order_id)
     );
     
 -->
@@ -19,12 +29,15 @@
 <?php include VIEW_PATH . 'templates/header_logined.php'; ?> <!--header-->
     <h1>購入履歴</h1>
     <?php include VIEW_PATH . 'templates/messages.php'; ?>
+    <?php if(count($carts) > 0){ ?>
       <table class="table table-bordered">
           <tr>
               <th>注文番号</th>
               <th>購入日時</th>
               <th>合計金額</th>
           </tr>
-      </table>        
+      </table> 
+
+    <?php } ?>         
 </body>
 </html>
