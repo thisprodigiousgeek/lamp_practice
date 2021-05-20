0a52fa55bdfd2649d907bc56330176f469de1e1a
tama <tamami.hmm@gmail.com>
2021-05-20 22:05:14 +0900

    adminに飛ばない

--- www/html/admin_change_status.php
+++ www/html/admin_change_status.php
@@ -17,13 +17,14 @@ $user = get_login_user($db);
 if(is_admin($user) === false){
   redirect_to(LOGIN_URL);
 }
+
 $post_token = get_post('token');//ポストで隠されて来たトークンにあだ名つける
 is_valid_csrf_token($post_token);//ポストで来たトークンをバリデする
 
 $item_id = get_post('item_id');
 $changes_to = get_post('changes_to');
 
-if($post_token !== $_SESSION['csrf_token']){//ポストされてきたトークンとッションに入ってるトークンが一致せんかったとき
+if(is_valid_csrf_token(get_post('token') === false){//ポストされてきたトークンがバリデしたけどfalseで返してきよったら（つまりポストされたやつとセッションに入ってるやつが一致せんかったら
   set_error('不正な処理が行われました');//セッション箱のエラーのとこに入れる
   $_SESSION = array();//セッション箱空にする
   redirect_to(LOGIN_URL);//ログインページに戻らせる
