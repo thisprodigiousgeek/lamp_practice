cc865a271cd0777b970b2fcad0ca79a7d6b490af
Tohgo Sasaki <tohhhhh@icloud.com>
2021-10-07 11:42:16 +0900

    xssの実装

--- www/view/finish_view.php
+++ www/view/finish_view.php
@@ -3,7 +3,7 @@
 <head>
   <?php include VIEW_PATH . 'templates/head.php'; ?>
   <title>ご購入ありがとうございました！</title>
-  <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'admin.css'); ?>">
+  <link rel="stylesheet" href="<?php print(h(STYLESHEET_PATH . 'admin.css')); ?>">
 </head>
 <body>
   <?php include VIEW_PATH . 'templates/header_logined.php'; ?>
@@ -26,18 +26,18 @@
         <tbody>
           <?php foreach($carts as $cart){ ?>
           <tr>
-            <td><img src="<?php print(IMAGE_PATH . $cart['image']);?>" class="item_image"></td>
-            <td><?php print($cart['name']); ?></td>
-            <td><?php print(number_format($cart['price'])); ?>円</td>
+            <td><img src="<?php print(h(IMAGE_PATH . $cart['image']));?>" class="item_image"></td>
+            <td><?php print(h($cart['name'])); ?></td>
+            <td><?php print(h(number_format($cart['price']))); ?>円</td>
             <td>
-                <?php print($cart['amount']); ?>個
+                <?php print(h($cart['amount'])); ?>個
             </td>
-            <td><?php print(number_format($cart['price'] * $cart['amount'])); ?>円</td>
+            <td><?php print(h(number_format($cart['price'] * $cart['amount']))); ?>円</td>
           </tr>
           <?php } ?>
         </tbody>
       </table>
-      <p class="text-right">合計金額: <?php print number_format($total_price); ?>円</p>
+      <p class="text-right">合計金額: <?php print h(number_format($total_price)); ?>円</p>
     <?php } else { ?>
       <p>カートに商品はありません。</p>
     <?php } ?> 
