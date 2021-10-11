f1c32f320435ba477c75e657bb1f9c33bc2c28c0
Tohgo Sasaki <tohhhhh@icloud.com>
2021-10-11 17:41:21 +0900

    新規テーブル追加

---
+++ lamp_dock/append_schema.sql
@@ -0,0 +1,23 @@
+-- 購入履歴テーブル
+-- 注文番号、user_id、購入日時
+
+CREATE TABLE `purchase_history`(
+    `order_id` int(11) NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL,
+    `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    primary key(order_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 購入明細テーブル
+-- 明細番号、注文番号、item_id、数量,小計,
+
+CREATE TABLE `purchase_detail`(
+    `detail_id` int(11) AUTO_INCREMENT,
+    `order_id` int(11) NOT NULL,
+    `item_id` int(11) NOT NULL,
+    `sub_total` int(11) NOT NULL,
+    `amount` int(11) NOT NULL,
+    primary key(detail_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
